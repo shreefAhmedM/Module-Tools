610bb3248ceeb8c6a6b1fda2ac0eb96a95e256c0
shreefAhmedM <livecoding.33@gmail.com>
2026-08-01 13:20:59 +0100

    completing cowsay exercies

---
+++ implement-cowsay/cow.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
+import cowsay
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Make animals say things")
+
+    parser.add_argument(
+        "--animal",
+        choices=cowsay.char_names,
+        default="cow",
+        help="The animal to be saying things.",
+    )
+
+    parser.add_argument(
+        "message",
+        nargs="+",
+        help="The message to say.",
+    )
+
+    args = parser.parse_args()
+
+    text = " ".join(args.message)
+    print(cowsay.get_output_string(args.animal, text))
+
+
+if __name__ == "__main__":
+    main()
