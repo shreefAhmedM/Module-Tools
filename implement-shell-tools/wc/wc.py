c881883af5e776c1d4123d8f3990c0f7d13978bb
shreefAhmedM <livecoding.33@gmail.com>
2026-08-07 21:14:40 +0100

    implementing shell tools command in python

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -1,4 +1,5 @@
 import argparse
+
 parser = argparse.ArgumentParser(prog="wc")
 parser.add_argument("-l", action="store_true", help="Count lines")
 parser.add_argument("-w", action="store_true", help="Count words")
@@ -6,6 +7,7 @@ parser.add_argument("-c", action="store_true", help="Count bytes")
 parser.add_argument("files", nargs="+")
 
 args = parser.parse_args()
+
 total_lines = 0
 total_words = 0
 total_chars = 0
@@ -15,31 +17,42 @@ for file in args.files:
         text = f.read()
 
     lines = text.count("\n")
-    words = len(text.split())
-    chars = len(text)
+
+    stripped = text.strip()
+    words = len(stripped.split()) if stripped else 0
+
+    chars = len(text.encode())
 
     total_lines += lines
     total_words += words
     total_chars += chars
 
+    output = ""
+
     if not args.l and not args.w and not args.c:
-        print(lines, words, chars, file)
+        output += f"{lines:8}{words:8}{chars:8}"
     else:
         if args.l:
-            print(lines, end=" ")
+            output += f"{lines:8}"
         if args.w:
-            print(words, end=" ")
+            output += f"{words:8}"
         if args.c:
-            print(chars, end=" ")
-        print(file)
+            output += f"{chars:8}"
+
+    print(output, file)
+
+
 if len(args.files) > 1:
+    output = ""
+
     if not args.l and not args.w and not args.c:
-        print(total_lines, total_words, total_chars, "total")
+        output += f"{total_lines:8}{total_words:8}{total_chars:8}"
     else:
         if args.l:
-            print(total_lines, end=" ")
+            output += f"{total_lines:8}"
         if args.w:
-            print(total_words, end=" ")
+            output += f"{total_words:8}"
         if args.c:
-            print(total_chars, end=" ")
-        print("total")
+            output += f"{total_chars:8}"
+
+    print(output, "total")
