4b082acd81b15e6d5594ad6d72ae4252cc6d5660
shreefAhmedM <livecoding.33@gmail.com>
2026-08-09 12:43:05 +0100

    Limits of type checking

---
+++ sprint5-prep-exercises/LimiOfTypeCheck.py
@@ -0,0 +1,7 @@
+def double(number):
+    # return number * 3
+    return number * 2
+
+print(double(10))
+# The bug is that the double function multiplies the number by 3 instead of 2.
+#  Since the function is called double, it should multiply the number by 2. 
