e437bab6decf912b75a64a95c9217ae22504a93a
shreefAhmedM <livecoding.33@gmail.com>
2026-08-09 16:53:22 +0100

    Running the code through mypy, and fix all of the bugs that show up

---
+++ sprint5-prep-exercises/typeAnnotations.py
@@ -0,0 +1,35 @@
+
+def open_account(balances: dict[str, int], name: str, amount: int):
+    balances[name] = amount
+
+
+def sum_balances(accounts: dict[str, int]) -> int:
+    total = 0
+    for name, pence in accounts.items():
+        print(f"{name} had balance {pence}")
+        total += pence
+    return total
+
+
+def format_pence_as_string(total_pence: int) -> str:
+    if total_pence < 100:
+        return f"{total_pence}p"
+    pounds = int(total_pence / 100)
+    pence = total_pence % 100
+    return f"£{pounds}.{pence:02d}"
+
+
+balances: dict[str, int] = {
+    "Sima": 700,
+    "Linn": 545,
+    "Georg": 831,
+}
+
+
+open_account(balances, "Tobi", 913)
+open_account(balances, "Olya", 713)
+
+total_pence = sum_balances(balances)
+total_string = format_pence_as_string(total_pence)
+
+print(f"The bank accounts total {total_string}")
