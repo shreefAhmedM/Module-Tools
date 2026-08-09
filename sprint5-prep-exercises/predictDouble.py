4a2895919564b1a4b0cc6e42dac362c712dfb202
shreefAhmedM <livecoding.33@gmail.com>
2026-08-09 12:30:51 +0100

    producting  what 2 * 22 will do in py

---
+++ sprint5-prep-exercises/predictDouble.py
@@ -0,0 +1,13 @@
+def half(value):
+    return value / 2 
+
+def double(value):
+    return value * 2 
+
+def second(value):
+    return value[1]
+
+
+print(double("22"))
+# I predicted that it would return an error, but it actually returned 2222.
+#  I didn't expect this because I thought double() was supposed to work only with numbers.
